0b7f1dd6f49f8f3005708f2b6a05834229a2b533
Daniel <danialups1997@gmail.com>
2020-10-05 15:19:25 +0200

    this solution is better

--- edabit/day-1/largest_swap.js
+++ edabit/day-1/largest_swap.js
@@ -1,17 +1,14 @@
 // https://edabit.com/challenge/hD3euqPHM82Cbr7R8
 
 /**
- * it can be solve in a better way i am sure, but that what i come up with for now
+ * 
  * @param {number} num
  */
 function largestSwap(num) {
-    var reverse = num.toString().split("").reverse().join("");
-    var res = parseInt(reverse, 10);
-    if (num > res) return true;
-    if (num < res) return false;
-    if (num === res) return true;
+    var reverse = Number(num.toString().split("").reverse().join(""));
+    return num >= reverse;
 }
-console.log(largestSwap(43));
+console.log(largestSwap(44));
 
 //---------- other solution ----------
 
