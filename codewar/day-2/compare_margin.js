90beb0e9677d11bdac21212e9ebf066dca4ea27a
Daniel <danialups1997@gmail.com>
2020-10-06 23:01:27 +0200

    see you at day 3

---
+++ codewar/day-2/compare_margin.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/56453a12fcee9a6c4700009c/train/javascript
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ * @param {number} margin 
+ * @returns {number}
+ */
+function closeCompare(a, b, margin){
+    
+}
