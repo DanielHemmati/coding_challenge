4e71734e75b73bbf53b4f29a31920d9615fdc468
Daniel <danialups1997@gmail.com>
2020-10-07 13:20:01 +0200

    second challenge,it was too easy

---
+++ edabit/day-3/perimeterRectangle.js
@@ -0,0 +1,15 @@
+
+// https://edabit.com/challenge/XnJ24rWW7iJkNrtsh
+
+// easy (which is not good )
+
+/**
+ * 
+ * @param {nubmer} length 
+ * @param {number} width 
+ */
+function findPerimeter(length, width) {
+    return 2 * (length + width);
+}
+
+console.log(findPerimeter(2,2));
