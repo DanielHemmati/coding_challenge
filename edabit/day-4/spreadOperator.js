15286620a103713683b68ae863b7f84b139f8a7a
Daniel <danialups1997@gmail.com>
2020-10-08 17:49:35 +0200

    i love this spread operator it's so good

---
+++ edabit/day-4/spreadOperator.js
@@ -0,0 +1,20 @@
+
+// just for leaning 
+
+const string = "daniel Hemmati"
+const char = [...string]
+
+console.log(char);
+
+var res = []
+for (var i = 0; i < string.length; i++) {
+    res.push(string.charAt(i))
+}
+console.log(res);
+
+const item = ['ths', 'is', 'a', 'sentence']
+console.log(item);
+console.log(...item);
+
+
+
