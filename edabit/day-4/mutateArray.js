0c50cbbfbdc0820f83973c3689105080eb6278af
Daniel <danialups1997@gmail.com>
2020-10-08 17:32:56 +0200

    we learn about mutation stuff

---
+++ edabit/day-4/mutateArray.js
@@ -0,0 +1,17 @@
+// https://edabit.com/challenge/j3EoCMaD3LrEYtx78
+
+// Fix this incorrect code, so that all tests pass!
+/**
+ *
+ * @param {Array} arr
+ */
+function minusOne(arr) {
+    var b = [...arr];
+    b.pop();
+    return b;
+}
+
+// var array = [1, 2, 3];
+// console.log(array.slice(0, -1));
+// console.log(array);
+
