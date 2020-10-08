16308dc21f0afd45f1a44f48dff2e0d5172fbabe
Daniel <danialups1997@gmail.com>
2020-10-08 17:40:39 +0200

    it was cool, learned more about spread operator

---
+++ edabit/day-4/maxDifference.js
@@ -0,0 +1,16 @@
+
+// https://edabit.com/challenge/4vvFr9ZTK2AdyswXH
+
+/**
+ * 
+ * @param {Number} nums 
+ * @returns {Number}
+ */
+function difference(nums) {
+    return Math.max(...nums) - Math.min(...nums);
+}
+
+var array = [1,2,3]
+// am i a good programmer now
+// console.log(difference(array));
+
