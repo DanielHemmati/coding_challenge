565bdae984cb8583b41d534d83dcb0bb06bedec2
Daniel <danialups1997@gmail.com>
2020-10-08 14:07:28 +0200

    now we go for hard challenge

---
+++ edabit/day-4/currnetVoltage.js
@@ -0,0 +1,8 @@
+// i just find out that it set my experince as beginner
+// that's why we saw easy problem
+
+function idk(voltage, current) {
+    return voltage * current;
+}
+
+console.log(idk(110, 3));
---
+++ edabit/day-4/hoursMinute.js
@@ -0,0 +1,12 @@
+
+// https://edabit.com/challenge/8q54MKnRrm89pSLmW
+
+/**
+ * 
+ * @param {Number} minutes 
+ */
+function convert(minutes) {
+    return minutes * 60;
+}
+
+console.log(convert(5));
