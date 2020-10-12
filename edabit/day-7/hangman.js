2f629265b6e8d8b180d8514f3746b1da32a8b0dd
Daniel <danialups1997@gmail.com>
2020-10-12 19:42:56 +0200

    it was hard :(

---
+++ edabit/day-7/hangman.js
@@ -0,0 +1,37 @@
+// https://edabit.com/challenge/Qc8k4WX2Gi4uSYR5A
+
+/**
+ *
+ * @param {string} phrase
+ * @param {Array} arr
+ */
+function hangman(phrase, arr) {
+    return [...phrase]
+        .map(function (char) {
+            return /[a-z]/i.test(char) && !arr.includes(char.toLowerCase())
+                ? "-"
+                : char;
+        })
+        .join("");
+}
+
+// research part right now it's day 8, but i am will put this on day7
+// it doesn't matrer
+
+var arr = ["a", "d"];
+var ph = "daniel";
+// console.log(hangman(ph, arr));
+
+/**
+ * 
+ * @param {String} phrase 
+ * @param {Array} arr 
+ */
+function hangman2(phrase, arr) {
+    return [...phrase].map(function(char) {
+        if (/[a-z]/i.test(char)) {
+            return !arr.includes(char.toLowerCase()) ? '-' : char;
+        }
+    }).join('')
+}
+console.log(hangman2(ph, arr));
