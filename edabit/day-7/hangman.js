2526f7499a9220b4317985a082b6bf5d414bb926
Daniel <danialups1997@gmail.com>
2020-10-13 18:25:33 +0200

    i commit this last day didn't i?

--- edabit/day-7/hangman.js
+++ edabit/day-7/hangman.js
@@ -23,15 +23,28 @@ var ph = "daniel";
 // console.log(hangman(ph, arr));
 
 /**
- * 
- * @param {String} phrase 
- * @param {Array} arr 
+ *
+ * @param {String} phrase
+ * @param {Array} arr
  */
 function hangman2(phrase, arr) {
-    return [...phrase].map(function(char) {
-        if (/[a-z]/i.test(char)) {
-            return !arr.includes(char.toLowerCase()) ? '-' : char;
-        }
-    }).join('')
+    return [...phrase]
+        .map(function (char) {
+            if (/[a-z]/i.test(char)) {
+                return !arr.includes(char.toLowerCase()) ? "-" : char;
+            }
+        })
+        .join("");
 }
 console.log(hangman2(ph, arr));
+
+var array = [0, 4, 3, 5];
+
+function randomArrayGenerator(item, hn) {
+    let array = [];
+    // this is so cool
+    for (let i = 0; i < item; ++i) {
+        array.push(Math.floor(Math.random() * hn))
+    }
+    return array;
+}
