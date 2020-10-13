7ebe74bfec35ddb416744f0ce5b3d99be36adf4b
Daniel <danialups1997@gmail.com>
2020-10-13 18:31:46 +0200

    this is so cool

---
+++ edabit/day-9/pi.js
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ * @param {number} n 
+ */
+function getPi(n) {
+    // formula https://en.wikipedia.org/wiki/Machin-like_formula
+    return (16 * Math.atan(1 / 5) - 4 * Math.atan(1 / 239)).toFixed(n)
+}
+
+// this is next level
+console.log(getPi(50));
