4508f1ceed772e19139c2199e8d658229eab9c3d
Daniel <danialups1997@gmail.com>
2020-10-17 20:36:46 +0200

    first challenge done, just think simple

--- edabit/day-13/onlyUppercase.js
+++ edabit/day-13/onlyUppercase.js
@@ -1,13 +1,42 @@
-
 // https://edabit.com/challenge/TDpT9tvwJK5uLn98h
+
+/**
+ *
+ * @param {String} str
+ */
+function sameCase(str) {
+    if (str.toUpperCase() === str) return true;
+    if (str.toLowerCase() === str) return true;
+    return false;
+}
+
+// console.log(sameCase('DANIr'));
+// research, exactly one hours on this. conclusion ==> think simple
+// * i have an idea. charcode from 65 utnil 90, all of them are uppercase
+// * or we can check if the string is equal to uppercase of itself
+// * or we can use regex
+// * lol it's much simpler than that 😂
+
+// var upperA = "A".charCodeAt(0); // 65
+// var upperZ = "Z".charCodeAt(0); // 90
+// var lowerA = "a".charCodeAt(0); // 97
+// var lowerZ = "z".charCodeAt(0); // 122
+//---------- othere solution ----------
 
 /**
  * 
  * @param {String} str 
  */
-function sameCase(str) {
-    
+function sameCase2(str) {
+    // much better
+    return str.toUpperCase() === str || str.toLowerCase() === str; 
 }
 
+// console.log(sameCase2('daniel'));
 
-// research
+// and yeah you can do it with regex 
+function sameCase3(str) {
+    // you have to say from beginning ^ to the end $ with +
+    return /^[A-Z]+$/g.test(str) ||  /^[a-z]+$/g.test(str)
+}
+console.log(sameCase3('danie'));
