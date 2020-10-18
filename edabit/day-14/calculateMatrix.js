9c696adca061249b52f1587e472229cecab7a129
Daniel <danialups1997@gmail.com>
2020-10-18 15:08:49 +0200

    make it tidier LOL

--- edabit/day-14/calculateMatrix.js
+++ edabit/day-14/calculateMatrix.js
@@ -6,11 +6,7 @@
  * @example [[a, b], [c, d]] ad-bc [[1,2], [3,4]] (1*3) - (3*2) = -2
  */
 function calcDeterminant(matrix) {
-    return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
+    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
 }
 
-
-console.log(calcDeterminant([
-  [1, 2]
-  [3, 4]
-]));
+console.log(calcDeterminant([[1, 2][(3, 4)]]));
