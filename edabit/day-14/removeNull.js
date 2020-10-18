fc0432f31bd910a6377fccbe94e22d41b657cf52
Daniel <danialups1997@gmail.com>
2020-10-18 14:47:24 +0200

    i mean the solution was awesome

---
+++ edabit/day-14/removeNull.js
@@ -0,0 +1,27 @@
+// https://edabit.com/challenge/2SJX9ha6W2bYkzSNX
+
+/**
+ * 
+ * @param {Array} arr 
+ */
+function removeNull(arr) {
+    return arr.filter(function(item) {
+        return item !== null;  
+    })
+}
+
+// today i am on fire man let's go. 
+// console.log(removeNull(["a", null, "b", null]));
+
+
+
+/**
+ * 
+ * @param {Array} arr 
+ */
+function removeNull2(arr) {
+    // really cool
+    return arr.filter(Boolean)
+}
+
+console.log(removeNull2(["a", null, "b", null]));
