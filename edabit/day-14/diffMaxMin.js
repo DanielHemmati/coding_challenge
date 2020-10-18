227839d7384dec0f08a76fba4923c3de0f7e7cb7
Daniel <danialups1997@gmail.com>
2020-10-18 14:52:08 +0200

    i feel like i solve this before, but that's okay

---
+++ edabit/day-14/diffMaxMin.js
@@ -0,0 +1,8 @@
+// https://edabit.com/challenge/v9DwaeR6NQoapEvHh
+
+function diffMaxMin(arr) {
+    return Math.max(...arr) - Math.min(...arr)
+}
+
+var arr = [22,3,1]
+console.log(diffMaxMin(arr));
