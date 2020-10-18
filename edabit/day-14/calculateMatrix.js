f7bb4eb83e09b6bb7e51261f4d1b2d6eb02d4fa6
Daniel <danialups1997@gmail.com>
2020-10-18 15:01:59 +0200

    relearn what've forgotten 😃

---
+++ edabit/day-14/calculateMatrix.js
@@ -0,0 +1,16 @@
+// https://edabit.com/challenge/MwLp6s75F6S5wRrEJ
+
+/**
+ *
+ * @param {Array} matrix
+ * @example [[a, b], [c, d]] ad-bc [[1,2], [3,4]] (1*3) - (3*2) = -2
+ */
+function calcDeterminant(matrix) {
+    return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
+}
+
+
+console.log(calcDeterminant([
+  [1, 2]😃
+  [3, 4]
+]));
