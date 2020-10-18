4e6db2983b669fc89666e473ecd6505de37169eb
Daniel <danialups1997@gmail.com>
2020-10-18 14:15:33 +0200

    first challenge of the day

---
+++ edabit/day-14/shuffleName.js
@@ -0,0 +1,11 @@
+// https://edabit.com/challenge/eitMRpuJR3Ls4zQsn
+
+/**
+ * 
+ * @param {String} str 
+ */
+function nameShuffle(str) {
+    return str.split(' ').reverse().join(' ')
+}
+
+console.log(nameShuffle('daniel hemmaitl'));
