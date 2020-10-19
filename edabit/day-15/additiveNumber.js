a2e8e08a8b69ef0a0c7be6d833b5f412bc18ad01
Daniel <danialups1997@gmail.com>
2020-10-19 16:43:30 +0200

    1st challenge of day 15

---
+++ edabit/day-15/additiveNumber.js
@@ -0,0 +1,11 @@
+// https://edabit.com/challenge/Qbi3GuXjwW5fsoKRn
+
+/**
+ * 
+ * @param {Array} arr 
+ */
+function additiveInverse(arr) {
+    return arr.map(number => number * -1)
+}
+
+console.log(additiveInverse([5, -7, 8, 3]));
