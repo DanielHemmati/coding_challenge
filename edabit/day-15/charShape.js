830a78631883cec135559fef0eabe9fff4db7b93
Daniel <danialups1997@gmail.com>
2020-10-19 17:01:10 +0200

    let's go, but it was easy

---
+++ edabit/day-15/charShape.js
@@ -0,0 +1,16 @@
+// https://edabit.com/challenge/nkp5xkZwbBH5Ape8b
+
+// i don't really understand the question. but for now i am
+// going to count the item 😅
+/**
+ *
+ * @param {Array} arr
+ */
+function countCharacters(arr) {
+    return arr.join('').length;
+}
+
+var test = ["###", "###", "###"];
+
+// fortunatly it was right 😏
+console.log(countCharacters(test));
