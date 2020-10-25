23ada894ebc6975896094d0589ab5db3c8573a1f
danielhemmati <danialups1997@gmail.com>
2020-10-25 22:56:11 +0100

    test didn't passed, because of space :(

--- edabit/day-20/mexicanWave.js
+++ edabit/day-20/mexicanWave.js
@@ -2,8 +2,28 @@
 
 /**
  * this is hard
- * @param {String} str 
+ * @param {String} str
  */
 function wave(str) {
-    
+    return str.split('').map(function (letter, index, arr) {
+        return `${arr.slice(0, index)}${letter.toUpperCase()}${arr.slice(index + 1, arr.length)}`.replace(/,/g, "")
+    })
 }
+// console.log('daniel'.charAt(0).toUpperCase());
+
+console.log(wave('blue'));
+
+
+const wave2 = (str) => {
+    return str.split('').map(function (letter, index, arr) {
+        // return `${arr.slice(index + 1, arr.lengthj)}`
+        return `${arr.slice(0, index)}${letter.toUpperCase()}${arr.slice(index+1, arr.length)}`.replace(/,/g, '')
+    })
+}
+// console.log(wave2('daniel'));
+var string = 'daniel hemmati'
+var res = string.split('').map(function (letter, index, arr) {
+    // this is really nice
+    return `${arr.slice(0, index)}${letter.toUpperCase()}${arr.slice(index + 1, arr.length)}`.replace(/,/g, '')
+})
+// console.log(res);
