2df8ad3701a57d6e60f72d0c5ca89f3ae88207ea
danielhemmati <danialups1997@gmail.com>
2020-10-29 16:54:59 +0100

    next one

---
+++ codewar/day-23/fixTheProblem.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/560ecf0cb040de130e00007d/train/javascript
+
+// we want 32 as an answer
+function orderOperations() {
+    // here is your answre
+    return (2 + 2) * (2 + 2) * 2;
+}
+
+console.log(orderOperations());
