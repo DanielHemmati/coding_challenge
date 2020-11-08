0d21f3c64acf4d06a08b74929032b5cd43fcead9
danielhemmati <danialups1997@gmail.com>
2020-11-08 18:27:57 +0100

    modified the ruby

--- codewar/day-34/map_ruby.rb
+++ codewar/day-34/map_ruby.rb
@@ -1,13 +1,9 @@
-
-array = ['dnaiel', 'hemmati', 'age']
-res = array.map{|x| x.upcase}
+array = ["dnaiel", "hemmati", "age"]
+res = array.map { |x| x.upcase }
 
 # puts res
 
-
 # this is the answer
-def map list, &block
-    return list.map(&block)
+def map(list, &block)
+  return list.map(&block)
 end
-
-
