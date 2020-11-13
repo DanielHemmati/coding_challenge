04ecb2f5c34317fcca748f496aa32c510e9ea8bc
danielhemmati <danialups1997@gmail.com>
2020-11-13 20:53:32 +0100

    next

--- edabit/day-39/isEqual.js
+++ edabit/day-39/isEqual.js
@@ -1,7 +1,20 @@
-
 function isEqual(num1, num2) {
     if (typeof num1 !== "number" || typeof num2 !== "number") return false;
     return num1 === num2;
 }
 
-console.log(isEqual(2,2))
+// console.log(isEqual(2,2))
+
+//------------- other solution -------------
+function isEqual2(n1, n2) {
+    return Number.isInteger(n1, n2) && n1 === n2;
+}
+
+// console.log(isEqual2(2, 2));
+
+function isEqual3(n1, n2) {
+    return typeof n1 === "string" || typeof n2 === "string" ? false : n1 === n2;
+}
+
+// console.log(isEqual3("2", 2))
+
