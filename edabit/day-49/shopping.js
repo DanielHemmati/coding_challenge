56ad3b147f588f1000abf6d8d43b0608aef2b2e8
danielhemmati <danialups1997@gmail.com>
2020-11-22 16:33:55 +0100

    okay

---
+++ edabit/day-49/shopping.js
@@ -0,0 +1,11 @@
+
+
+// this is just for learnirg 
+function test(n) {
+  return  n
+}
+
+var a = {person1: ['daneil', 'hemmati'], person2:['shahin', 'good boy']}
+
+// okay we can do that tool 
+console.log(test(a['person1'][0]))
