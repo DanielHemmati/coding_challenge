8a726b40658e63154053a8ef26de1338457de4c3
danielhemmati <danialups1997@gmail.com>
2020-11-23 20:56:29 +0100

    somethis i didn't know about

---
+++ day-50/jsObject.js
@@ -0,0 +1,13 @@
+
+// it's okay to write them likt this, so we are cool 
+var test = {
+  name: 'ddaneil',
+  lname: 'hemmati'
+}
+
+
+// console.log(test.name)
+// this is like python and ruby
+// console.log(test['name'])
+// what the use of that
+
