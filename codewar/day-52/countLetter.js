91135a919d5f2a114a0ecfc4d6e63271da4b6679
danielhemmati <danialups1997@gmail.com>
2020-11-25 15:36:15 +0100

    this solution are amazing

--- codewar/day-52/countLetter.js
+++ codewar/day-52/countLetter.js
@@ -14,4 +14,24 @@ var  x ="abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"
 var a = "ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~"
 var y = ''
 // var res = str.match(/[a-zA-z]/gi).length
-console.log(lowercaseCount(a))
+// console.log(lowercaseCount(a))
+
+//------------- other solution -------------
+
+// this one is my fav
+/**
+ * 
+ * @param {String} str 
+ */
+function first(str) {
+  // i could do this but i didn't knew this 
+  return (str.match(/[a-z]/) || []).length
+}
+
+
+function second(str){
+  // yeah we could also do this 
+  return str.replace(/[^a-z]/g, "").length;
+}
+
+
