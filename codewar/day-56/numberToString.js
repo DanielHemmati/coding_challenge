102a43078558f90a67a5c98f86d2162698e216e6
danielhemmati <danialups1997@gmail.com>
2020-11-29 17:31:46 +0100

    it wasn't good TBH

---
+++ codewar/day-56/numberToString.js
@@ -0,0 +1,6 @@
+var a = 123;
+a = a + "";
+console.log(a);
+// there is no difference between that above and this below
+// but for some awful reason the test didn'accept this below one??
+console.log(a.toString());
