f3f61933944cf4ac469ac3116cd6595cbf74ef99
danielhemmati <danialups1997@gmail.com>
2021-01-03 14:27:34 +0100

    next time maybe, let's move on to the reactjs

---
+++ codeforce/day-87/watermelon.js
@@ -0,0 +1,10 @@
+// let's see how they are
+// you have to get the input from the user
+
+// it didn't work and i don't know why LOL
+var n = prompt();
+if (n % 2 == 0 && n !== 2) {
+  console.log("YES");
+} else {
+  console.log("NO");
+}
