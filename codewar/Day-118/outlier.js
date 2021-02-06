e9a1153410aa4f12b6a77f18cb367625882958b5
danielhemmati <danialups1997@gmail.com>
2021-02-06 13:51:33 +0100

    let's go, from now on i want to solve them in two language

---
+++ codewar/Day-118/outlier.js
@@ -0,0 +1,38 @@
+//https://www.codewars.com/kata/5526fc09a1bbd946250002dc/train/javascript
+/**
+ *
+ * @param {Array} integers
+ */
+function findOutlier(integers) {
+  var oddNumber = 0;
+  var evenNumber = 0;
+  for (let i = 0; i < integers.length; ++i) {
+    if (integers[i] % 2 === 0) {
+      evenNumber++;
+    } else {
+      oddNumber++;
+    }
+  }
+  if (evenNumber > oddNumber) {
+    return integers[integers.findIndex((item) => item % 2 !== 0)];
+  } else if (oddNumber > evenNumber) {
+    return integers[integers.findIndex((item) => item % 2 === 0)];
+  }
+}
+
+// console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
+
+// i think we can solve this with filter too
+// let's test this
+/**
+ *
+ * @param {Array} int
+ */
+function findOutlier2(int) {
+  return int.filter((item) => item % 2 === 0).length >
+    int.filter((item) => item % 2 !== 0).length
+    ? int.filter((item) => item % 2 !== 0)[0]
+    : int.filter((item) => item % 2 === 0)[0];
+}
+
+console.log(findOutlier2([2, 4, 0, 100, 4, 11, 2602, 36]));
