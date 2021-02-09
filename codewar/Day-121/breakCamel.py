0beeded6d26a145d774a4e06c60eb4aaaa9f2eff
danielhemmati <danialups1997@gmail.com>
2021-02-09 20:08:22 +0100

    let's go for learning some node js and expressjs

---
+++ codewar/Day-121/breakCamel.py
@@ -0,0 +1,32 @@
+# https://www.codewars.com/kata/5208f99aee097e6552000148/train/python
+
+import re
+
+
+def solution(s):
+    return re.sub(r"([A-Z])", r" \g<1>", s)
+    # or this
+    # return re.sub(r"([A-Z])", r" \1", s)
+
+
+# print(solution("danielHemmati"))
+
+# ------------------------------- with for loop ------------------------------ #
+
+def solution2(s):
+    res = ""
+    letter = [char for char in s]
+    for i in range(len(letter)):
+        if (letter[i] == letter[i].upper()):
+            res += " " + letter[i]
+        else:
+            res += letter[i]
+    return res
+
+
+print(solution2("danielHemmati"))
+
+
+test = "some string in here"
+res = [char for char in test]
+print(range(len(test)))
---
+++ mumbling.js
@@ -0,0 +1,55 @@
+// https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript
+/**
+ *
+ * @param {String} s
+ */
+// that was nice
+function accum(s) {
+  var res = [];
+  for (let i = 0; i < s.length; ++i) {
+    res.push(s[i].repeat(i + 1));
+  }
+  return res
+    .map((item) => {
+      return item.charAt(0).toUpperCase() + item.toLowerCase().slice(1);
+    })
+    .join("-");
+}
+
+// console.log(accum("MjtHuBovqr"));
+
+/* ----------------------------- other solution ----------------------------- */
+
+/**
+ *
+ * @param {String} s
+ */
+function second(s) {
+  return [...s]
+    .map((char, index) => {
+      // that's really nice technique
+      return char.toUpperCase() + char.toLowerCase().repeat(index);
+    })
+    .join("-");
+}
+
+// console.log(second("abcd"));
+
+/**
+ *
+ * @param {String} s
+ */
+function third(s) {
+  var array = [];
+  for (let i = 0; i < s.length; ++i) {
+    // repeat zero is nothing, wow that nice
+    array.push(s.charAt(i).toUpperCase() + s.charAt(i).toLowerCase().repeat(i));
+  }
+  console.log(array);
+}
+
+// console.log(third("abcd"));
+
+let test = "test";
+
+console.log(test.repeat(0));
