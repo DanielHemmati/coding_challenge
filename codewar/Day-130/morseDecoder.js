bf01e00ac4cd47797e4b56f912da94e7a72afd22
danielhemmati <danialups1997@gmail.com>
2021-02-18 14:55:35 +0100

    two solution my boy

---
+++ codewar/Day-130/morseDecoder.js
@@ -0,0 +1,94 @@
+let MORSE_CODE = {
+  "-.-.--": "!",
+  ".-..-.": '"',
+  "...-..-": "$",
+  ".-...": "&",
+  ".----.": "'",
+  "-.--.": "",
+  "-.--.-": "",
+  ".-.-.": "+",
+  "--..--": ",",
+  "-....-": "-",
+  ".-.-.-": ".",
+  "-..-.": "/",
+  "-----": "0",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "---...": ":",
+  "-.-.-.": ";",
+  "-...-": "=",
+  "..--..": "?",
+  ".--.-.": "@",
+  ".-": "A",
+  "-...": "B",
+  "-.-.": "C",
+  "-..": "D",
+  ".": "E",
+  "..-.": "F",
+  "--.": "G",
+  "....": "H",
+  "..": "I",
+  ".---": "J",
+  "-.-": "K",
+  ".-..": "L",
+  "--": "M",
+  "-.": "N",
+  "---": "O",
+  ".--.": "P",
+  "--.-": "Q",
+  ".-.": "R",
+  "...": "S",
+  "-": "T",
+  "..-": "U",
+  "...-": "V",
+  ".--": "W",
+  "-..-": "X",
+  "-.--": "Y",
+  "--..": "Z",
+  "..--.-": "_",
+  "...---...": "SOS",
+};
+
+/**
+ *
+ * @param {String} morseCode
+ */
+const decodeMorse = function (morseCode) {
+  // idk why we do get number
+  const decodeMorse = morseCode
+    .split("   ")
+    .map((word) => {
+      return word
+        .split(" ")
+        .map((char) => {
+          return MORSE_CODE[char];
+        })
+        .join("");
+    })
+    .join(" ");
+  return decodeMorse.trim();
+};
+
+console.log(decodeMorse(".... . -.--   .--- ..- -.. ."));
+/**
+ *
+ * @param {String} morseCode
+ */
+const secondAttempt = function (morseCode) {
+  function decodeMorseLetter(letter) {
+    return MORSE_CODE[letter];
+  }
+  function decodeMorseWord(word) {
+    return word.split(" ").map(decodeMorseLetter).join("");
+  }
+  return morseCode.trim().split("   ").map(decodeMorseWord).join(" ");
+};
+
+console.log(secondAttempt(".... . -.--   .--- ..- -.. ."));
