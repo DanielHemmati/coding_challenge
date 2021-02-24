f69846df286793432d3be5483e1054e21de8930f
danielhemmati <danialups1997@gmail.com>
2021-02-25 00:21:39 +0100

    okay it's 00:21 right now, time for sleep

---
+++ codewar/Day-136/arrayDif.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * @param {Array} a
+ * @param {Array} b
+ */
+function arrayDiff(a, b) {
+  return a.filter((item, index) => item !== b) || [];
+}
+
+// console.log(arrayDiff([], [3]));
+// console.log(arrayDiff([1, 8, 2], []));
+// console.log([1, 2, "daniel"].indexOf("daniel") !== );
+// console.log(arrayDiff([5, 19, -11], [-11, 19, 5]));
+
+/* ----------------------------- other solution ----------------------------- */
+// fuck the last minute i just thought about this, NOOOOOO
+// i had the idea but i was lazy
+/**
+ *
+ * @param {Array} a
+ * @param {Array} b
+ */
+function second(a, b) {
+  return a.filter((item) => !b.includes(item));
+}
+
+console.log(second([], [3]));
+console.log(second([1, 8, 2], []));
+console.log(second([5, 19, -11], [-11, 19, 5]));
