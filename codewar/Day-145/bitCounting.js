148b08709bd9447de5551523038f331e1c61015a
danielhemmati <danialups1997@gmail.com>
2021-03-05 13:39:01 +0100

    nice

---
+++ codewar/Day-145/bitCounting.js
@@ -0,0 +1,23 @@
+// https://www.codewars.com/kata/526571aae218b8ee490006f4/train/javascript
+
+/**
+ *
+ * @param {number} n
+ */
+const countBits = (n) => {
+  // const nBit = n.toString(2);
+  // console.log(nBit);
+  // return nBit.split("").filter((item) => {
+  //   return Number(item === "1");
+  // }).length;
+  return n
+    .toString(2)
+    .split("")
+    .filter((item) => Number(item === "1")).length;
+};
+
+const test = 123;
+// console.log(test.toString(2));
+// console.log((test >>> 0).toString(2));
+
+console.log(countBits(test));
