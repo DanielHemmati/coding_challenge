4628bf321b414df8d0eb58aa63c29431f892b480
danielhemmati <danialups1997@gmail.com>
2021-03-06 18:35:50 +0100

    okay this one was interesting

---
+++ codewar/Day-146/alphabetPositoin.py
@@ -0,0 +1,22 @@
+
+
+# i saw the solution, still i have problem with python
+def alphabet_position(text):
+    res = ""
+    text_uppercase = text.upper()
+    for char in text_uppercase:
+        if ord(char) > 64 and ord(char) < 91:
+            res += str(ord(char)) - 64 + " "
+
+    return res
+
+
+a = "daenil"
+# print(alphabet_position(a))
+
+
+def alphabet(text):
+    return " ".join(str(ord(char) - 96) for char in text.lower() if char.isalpha())
+
+
+print(alphabet(a))
