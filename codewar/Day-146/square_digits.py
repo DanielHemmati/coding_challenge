d44034ce0e94ab0b81c90982c245ff5b43a2de90
danielhemmati <danialups1997@gmail.com>
2021-03-06 18:58:15 +0100

    i have to work on my python skill man

---
+++ codewar/Day-146/square_digits.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/546e2562b03326a88e000020/train/python
+
+
+def square_digits(num):
+    str_n = list(str(num))
+    for number in str_n:
+        # print(int(number) ** 2, end="")
+        res += int(number)**2 + ""
+    return res
+
+
+x = 9119
+print(square_digits(x))
