52a9f63f33cbfebb3ac5262cd5058f87db63b691
danielhemmati <danialups1997@gmail.com>
2021-03-14 16:59:21 +0100

    i want more

---
+++ codewar/Day-154/equalSide.js
@@ -0,0 +1,45 @@
+// https://www.codewars.com/kata/5679aa472b8f57fb8c000047/train/javascript
+
+/**
+ *
+ * @param {Array} arr
+ */
+// this is hard
+// over all i didn't like this problem
+function findEvenIndex(arr) {
+  if (arr.length === 1) return 0;
+  for (let i = 1; i < arr.length - 1; ++i) {
+    const leftSum = arr.slice(0, i).reduce((a, b) => a + b, 0);
+    const rightSum = arr.slice(i + 1).reduce((a, b) => a + b, 0);
+    console.log("sum of the left side " + leftSum);
+    console.log("sum of the right side " + rightSum);
+    if (leftSum === rightSum) return i;
+  }
+  return -1;
+}
+
+let a = [0, 8];
+let c = [20, 10, -80, 10, 10, 15, 35];
+// console.log(findEvenIndex(c));
+
+const sum = (a, from, to) => a.slice(from, to).reduce((a, b) => a + b, 0);
+const findEvenIndex2 = (array) =>
+  array.findIndex((_, i) => sum(array, 0, i) === sum(array, i + 1));
+
+// let b = [1, 2, 3, 4, 20];
+// let res = b.findIndex((a, i) => a === 20);
+// console.log(res);
+
+/**
+ *
+ * @param {Array} arr
+ */
+function secondSolution(arr) {
+  return arr.findIndex(
+    (_, i) =>
+      arr.slice(0, i).reduce((a, b) => a + b, 0) ===
+      arr.slice(i + 1).reduce((a, b) => a + b, 0)
+  );
+}
+
+console.log(secondSolution(a));
---
+++ codewar/Day-154/set_alarm.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/568dcc3c7f12767a62000038/solutions/python
+
+# practicing python easy problem
+
+def set_alarm(employed, vacation):
+    # you can also write this with ifelse statement like this
+    # if employed == True and vacation == False:
+    #     return True
+    # else:
+    #     return False
+    #
+    return employed and not vacation
+
+# for js is like this
+# function test(em, vac){
+#     return em && !vac
+# }
