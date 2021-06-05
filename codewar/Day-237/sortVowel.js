ba6703e6b03afde21cb79e85fc3689588b9030dc
Daniel Hemmati <danialups199@gmail.com>
2021-06-05 21:08:36 +0200

    forgot it

---
+++ codewar/Day-237/sortVowel.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/59e49b2afc3c494d5d00002a/train/javascript
+
+/**
+ *
+ * @param {String} s
+ */
+function sortVowels(s) {
+  let regex = /aeiou/g;
+}
+
+let regex = /[aeiou]/g;
+let test = "daniel";
+console.log(test.match(regex));
