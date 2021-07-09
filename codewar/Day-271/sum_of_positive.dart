738ca98b6a40ea51d4992bfacc725fdf035fc949
Daniel Hemmati <danialups199@gmail.com>
2021-07-09 13:50:18 +0200

    last solution, wow: :

--- codewar/Day-271/sum_of_positive.dart
+++ codewar/Day-271/sum_of_positive.dart
@@ -1,6 +1,10 @@
+import "dart:math";
+
 void main() {
-  var list = [1, 2, 3, -2];
-  print(positiveSum(list));
+  var list = <int>[1, 2, 3, -2];
+  // print(positiveSum(list));
+  // print(max(1, 2));
+  print(clever(list));
 }
 
 int positiveSum(List<int> arr) {
@@ -8,4 +12,10 @@ int positiveSum(List<int> arr) {
       .where((element) => element > 0)
       .toList()
       .fold(0, (previousValue, element) => previousValue + element);
+}
+
+/* ----------------------------- the clever one----------------------------- */
+int clever(List<int> arr) {
+  return arr.fold(
+      0, (previousValue, element) => previousValue + max(element, 0));
 }
