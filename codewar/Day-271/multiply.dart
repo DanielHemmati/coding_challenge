1a04e7026479f313509d18c82a741ff8164f88bb
Daniel Hemmati <danialups199@gmail.com>
2021-07-09 13:03:20 +0200

    let's solve codewar, shall we

---
+++ codewar/Day-271/multiply.dart
@@ -0,0 +1,7 @@
+void main() {
+  print(multiply(2, 3));
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
---
+++ exercism/dart/resistor-color-duo/lib/resistor_color_duo.dart
@@ -0,0 +1,20 @@
+void main() {
+  final list = [1, 2, 3];
+  // codewar is much easier at fisrt, for me atleast
+}
+
+class ResistorColorDuo {
+  // Put your code here
+  final colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
+  ];
+}
