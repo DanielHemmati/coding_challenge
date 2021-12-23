456856c6fc74fb091ebbdaf67a3dcc1a7e5ee26d
danielhemmati <danialups1997@gmail.com>
2021-12-23 13:26:46 +0100

    there are many ways to add, subtract, mul, div

---
+++ codewar/Day-255/grow.py
@@ -0,0 +1,35 @@
+from functools import reduce
+from operator import add
+from operator import mul
+from math import prod
+
+
+def grow(arr: list) -> int:
+    res = 1
+    for i in arr:
+        res *= i
+    return res
+
+
+# print(grow([1, 2, 3]))
+
+# ------------------------------ other solution ------------------------------ #
+# python also have reduce, let's go
+
+def my_add(a: int, b: int) -> int:
+    return a + b
+
+
+number = [i for i in range(1, 10)]
+# print(reduce(my_add, number, 0))
+# print(reduce(lambda a, b: a + b, number, 0))
+# this one is next level
+# print(reduce(add, number))
+# print(sum(number))
+# print(reduce(mul, number))
+# print(reduce(prod, number))
+# print(prod(number))
+
+min_value, another_value, *rest = number
+# that's really cool
+print(rest)
