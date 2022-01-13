867aed5387f9cbfd5c4c7b02f4647c90998b3b2b
danielhemmati <danialups1997@gmail.com>
2022-01-13 21:20:49 +0100

    a lot of mistake just for one problem

---
+++ codewar/Day-457/diff.py
@@ -0,0 +1,9 @@
+def recur(p0, precent, aug, p, count = 0):
+    if p0 >= p:
+        return count
+    else: 
+        count += 1
+        pop = int(p0*(1 + precent/100)) + aug
+        return recur(pop, precent, aug, p, count)
+
+print(recur(1500, 5, 100, 5000))
---
+++ codewar/Day-457/test.py
@@ -0,0 +1,10 @@
+def nb_year(p0, percent, aug, p, count = 0):
+    if (p0 >= p):
+        return count
+    else:
+        count += 1
+        pop = p0 + p0 * (percent/100) + aug
+        return nb_year(pop, percent, aug, p, count)
+
+
+print("First", nb_year(1500, 5, 100, 5000))
