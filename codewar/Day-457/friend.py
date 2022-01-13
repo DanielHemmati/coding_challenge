9070a1d539baad2850f551bc4541807a84bbbb87
danielhemmati <danialups1997@gmail.com>
2022-01-13 22:53:00 +0100

    why list(filter()) ??

---
+++ codewar/Day-457/friend.py
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/55b42574ff091733d900002f/train/python
+
+def friend(x):
+    return [f for f in x if len(f) == 4]
+
+a = ["Ryan", "Jimmy", "123", "4", "Cool Man"]
+print(list(filter(lambda x: len(x) == 4, a )))
+print(friend(a))
