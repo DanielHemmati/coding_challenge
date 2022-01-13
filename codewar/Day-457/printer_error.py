b7f2d316467230a9bcb300f3cc96fbfd1e6e65de
danielhemmati <danialups1997@gmail.com>
2022-01-13 21:53:05 +0100

    this two way great

---
+++ codewar/Day-457/printer_error.py
@@ -0,0 +1,29 @@
+# https://www.codewars.com/kata/56541980fa08ab47a0000040/train/python
+
+# this is hard for me 🥴
+from re import sub
+import re
+
+
+def printer_error(s):
+    not_allowed = "nopqrstuvwxyz"
+    count = 0
+    for char in s:
+        if char in not_allowed:
+            count += 1
+    return str(f"{count}/{len(s)}")
+
+
+# print(printer_error('aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz'))
+# print(printer_error('kkkwwwaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyzuuuuu'))
+# print([x for x in "mnopqrstuvwxyz"])
+# print(len("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"))
+# print(len("mmmmmmmmmmmmmmmmmmmxyz"))
+
+
+#this is way more elegant 
+def printer_error2(s):
+    return "{}/{}".format(len(sub("[a-m]", "", s)), len(s))
+
+# love it
+print(printer_error2('aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz'))
---
+++ codewar/Day-457/sum_two_min.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/python
+
+def sum_two_smallest_numbers(numbers: list):
+    numbers.sort()
+    return numbers[0] + numbers[1]
+
+
+# print(sum_two_smallest_numbers([5, 8, 12, 18, 22]))
+
+# second solution 
+def foo(numbers: list):
+    return sum(sorted(numbers)[:2])
+
+print(foo([5, 8, 12, 18, 22]))
+
+print(len("aaaxbbbbyyhwawiwjjjwwm"))
