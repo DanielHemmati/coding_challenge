3144ae6fc03514a73d8ff2bae2d3515cda03aeaa
danielhemmati <danialups1997@gmail.com>
2022-01-24 17:38:18 +0100

    for day 465 done

--- codewar/Day-465/binary.py
+++ codewar/Day-465/binary.py
@@ -1,11 +1,9 @@
 # https://www.codewars.com/kata/551f37452ff852b7bd000139/train/python
 
-from audioop import add
+
 
 
 def add_binary(a,b):
-        #   bin(int(a,2) + int(b,2))
-    return bin(int(str(a), 2) + int(str(b) + 2))
+    return "{0:b}".format(a+b)
 
-
-print(add_binary(1,1))
+print(add_binary(5,1))
