f4d476c71e350028796b4574372db8b4888f167c
danielhemmati <danialups1997@gmail.com>
2022-01-28 11:52:32 +0100

    time for dijkstra algorithm

---
+++ codewar/6-kyu/smiley_face.py
@@ -0,0 +1,24 @@
+# https://www.codewars.com/kata/583203e6eb35d7980400002a/train/python
+
+# solved
+def count_smileys(arr):
+    if len(arr) == 0:
+        return 0
+    
+    valid = ":-) :-D :~) :~D ;-) ;-D ;~) ;~D :) :D ;) ;D".split(' ')
+    count = 0
+    for char in arr:
+        if char in valid:
+            count += 1
+
+    return count
+
+test = [';-(', ':D', ';(', ';o(', ';D', ':D', ';-D', ';-(', ':-D', ';oD', ';D', ':-D', ':D', ';D']
+# print(count_smileys(test))
+
+# other solutions
+import re
+def with_regex(arr: list):
+    return len(re.findall(r"[;:][-~]?[)D]", " ".join(arr)))
+
+print(with_regex(test))
--- codewar/6-kyu/split_string.py
+++ codewar/6-kyu/split_string.py
@@ -1,8 +1,34 @@
 
-a_string = "abcde"
+# solved
+def solution(string: str) -> list[str]:
+    if len(string) == 0:
+        return []
 
+    
+    res = []
+    for char in range(0, len(string), 2):
+        res.append(string[char:char+2])
+
+    if len(res[len(res) - 1]) == 1:
+        res[len(res) - 1] += "_"
+    
+    return res
+
+
+
+a_string = "abcdeb"
 n = 2
 split_strings = [a_string[index : index + n] for index in range(0, len(a_string), n)]
 
 split_strings[len(split_strings) - 1] += "_"
-print(split_strings)
+# print(split_strings[len(split_strings) - 1])
+# print(split_strings)
+# print(solution(""))
+
+# ------------------------------ other solutoin ------------------------------ #
+# with regex
+import re
+def withRegex(string: str) -> list[str]:
+    return re.findall(".{2}", string + "_")
+b = "abcd"
+print(withRegex(b))
