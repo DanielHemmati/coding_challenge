f8f69479845d9c2dfc9275dc4c8bfa4fe99df12e
DanielCodex <danialups1997@gmail.com>
2022-05-24 20:33:42 +0200

    my solution to squareDdigi.ts

---
+++ codewar/5-kyu/squreDdigit.ts
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/546e2562b03326a88e000020/train/typescript
+
+export class Kata {
+  static squareDigits(num: number): number {
+    let numToString = String(num);
+    let res = '';
+    for (let i = 0; i < numToString.length; ++i){
+      res += String(Math.pow(Number(numToString[i]), 2));
+    }
+    return +res;
+  }
+}
+
+
+let input = 9119;
+
+// console.log(Kata.squareDigits(input));
+
+
+
