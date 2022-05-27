e74f62b7a4a56b7aef20080158b087131bc8d15e
DanielCodex <danialups1997@gmail.com>
2022-05-27 13:38:00 +0200

    i like that recursion one

---
+++ codewar/6-kyu/subDigits.py
@@ -0,0 +1,37 @@
+
+
+import re
+from traceback import print_tb
+
+
+def digital_root(n):
+    if len(str(n)) == 1:
+        return n
+
+    return digital_root(sum([int(i) for i in str(n)]))
+
+
+a = 123
+res = sum([int(i) for i in str(a)])
+print(digital_root(a))
+
+# other solutio
+
+
+def second(n: int) -> int:
+    # i don't know why he wrote it like this
+    return (n % 9) or n and 9
+
+
+# print(second(a))
+
+def third(n: int) -> int:
+    res = lambda n: n and (n-1) % 9 + 1
+    return res
+
+
+print(third(a))
+
+digital_root2 = lambda n: n and (n - 1) % 9 + 1
+print(digital_root2(a))
+
