5cb064aa71cf9fc3e8dc0cb31231a6e5c8b45125
DanielCodex <danialups1997@gmail.com>
2022-06-06 18:38:38 +0200

    bc i want ignore exe of c and cpp
    but still have all the other langauges

--- codewar/8-kyu/intToString.c
+++ codewar/8-kyu/intToString.c
@@ -1,8 +1,14 @@
 // https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/c
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 int main() {
-  sprintf("a", "%d", 43);
+  // sprintf("a", "%d", 43);
+  printf("--------------------\n");
+  char str[255];
+
+  printf("%s\n", str);
   return 0;
 }
