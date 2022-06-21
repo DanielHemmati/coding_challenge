9f2cb648e2eafc361906c40059cbb4213c259beb
DanielCodex <danialups1997@gmail.com>
2022-06-21 17:45:00 +0200

    that a lot of fun

--- codewar/5-kyu/validParen.js
+++ codewar/5-kyu/validParen.js
@@ -7,12 +7,12 @@
  */
 // for some reason i couldn't solve this 🙃
 function validParentheses(parens) {
-  let regex = /\(\)/g
+  let regex = /\(\)/g;
   while (regex.test(parens)) {
     parens = parens.replace(regex, "");
   }
 
-  return !parens
+  return !parens;
 }
 
 // let a = ")(()))".split("()");
@@ -20,8 +20,91 @@ function validParentheses(parens) {
 // let c = b.join("").split("()");
 // console.log(c)
 
-let input = "((()))";
-console.log(validParentheses(input));
+// let input = "((()))";
+// let wrong = "()))";
+// console.log(validParentheses(input));
 
 // let test = "())(";
 // console.log(new Set(test));
+
+// other solution
+// i had one solution like this but it didn't worked
+// and with this solution the order of paren are not important
+// really clever
+function second(parens) {
+  var n = 0;
+  for (var i = 0; i < parens.length; i++) {
+    if (parens[i] == "(") n++;
+    if (parens[i] == ")") n--;
+    if (n < 0) return false;
+  }
+  return n == 0;
+}
+
+// console.log(second(input))
+
+/**
+ *
+ * @param {String} parens
+ * it's like the first one, a little bit simpler
+ */
+function third(parens) {
+  while (parens.indexOf("()") != -1) {
+    parens = parens.replace("()", "");
+  }
+
+  return !parens.length ? true : false;
+  // return !parens.length; // the sol was like this
+  // but i like the above one
+}
+
+// console.log(third(inputj));
+
+/**
+ *
+ * @param {String} parens
+ * @returns
+ */
+// i really like this
+function fourth(parens) {
+  let stack = [];
+  let i;
+
+  for (i = 0; i < parens.length; i++) {
+    if (parens[i] === "(") {
+      stack.push(parens[i]);
+      // stack.pop() will remove from array. i thought it won't work it won't mutate the array in the if statement
+    } else if ("(" !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+}
+
+let input = "((()))";
+let wrong = "()))";
+
+/**
+ *
+ * @param {String} paren
+ */
+// this is way simpler
+function fifth(paren) {
+  while (paren.includes("()")) {
+    paren = paren.replace("()", "");
+  }
+
+  return paren.length === 0;
+}
+
+// console.log(fifth(input));
+
+/**
+ *
+ * @param {String} parent
+ */
+function sixth(parens) {
+  return [...parens].reduce((a, b) => (a + b).replace("()", ""), "").length === 0;
+}
+console.log(sixth(input));
---
+++ codewar/5-kyu/validParen.py
@@ -0,0 +1,46 @@
+# https://www.codewars.com/kata/52774a314c2333f0a7000688/train/python
+from distutils import core
+import re
+
+# learnt really well from JS
+def valid_parentheses(string: str):
+    count = 0
+    for paren in string:
+        print(paren)
+        if paren == "(":
+            count += 1
+        if paren == ")":
+            count += -1
+        if count < 0:
+            return False
+
+    return count == 0
+
+
+correct = "((()))"
+wrong = "()))"
+
+# print(valid_parentheses("("))
+
+# solution
+
+
+def second(paren: str) -> bool:
+    try:
+        re.compile(paren)
+    except: 
+        return False
+
+    return True
+
+
+# print(re.compile(wrong))
+
+def third(paren: str) -> bool:
+    # paren = "".join(char for char in paren if char in "()")
+    while "()" in paren:
+        paren = paren.replace("()", "")
+    
+    return not paren
+
+print(third(wrong))
