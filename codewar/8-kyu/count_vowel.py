404699ac814f0357ddae2542feeb13d8d6354419
DanielCodex <danialups1997@gmail.com>
2022-07-25 22:48:07 +0200

    really interesting use of const 1

---
+++ codewar/8-kyu/count_vowel.py
@@ -0,0 +1,23 @@
+# https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/python
+
+
+def get_count(s: str) -> int:
+    res = 0
+    for char in s:
+        if char in "aeoiu":
+            res += 1
+
+    return res
+
+
+a = "aeiou"
+print(get_count(a))
+
+# ----------------- sol -----------------
+
+
+def first(s: str) -> int:
+    # return [1 for char in s if s in "aeiou"]
+    return sum(1 for char in s if s in "aeiou")
+
+print(first(a))
