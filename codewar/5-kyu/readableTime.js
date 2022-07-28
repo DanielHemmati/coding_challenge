c701126bc6fd83209b8b5d229992ecbee7092ed2
DanielCodex <danialups1997@gmail.com>
2022-07-28 23:23:01 +0200

    couldn't solve it for js, will train later

--- codewar/5-kyu/readableTime.js
+++ codewar/5-kyu/readableTime.js
@@ -1,12 +1,31 @@
 /**
- * 
- * @param {number} seconds 
+ *
+ * @param {number} time
  * @returns {string}=> am i
  */
-function humanReadable(seconds) {
-  let test = seconds % 60;
-  console.log(test)
-  return 
+function humanReadable(time) {
+  let hours = parseInt(time / 3600);
+  let minute = parseInt(time / 60) % 60;
+  let second = time % 60;
+
+  const pad = (val) => {
+    return val < 10 ? "0" + val : val;
+  };
+  return `${pad(hours)}:${pad(minute)}:${pad(second)}`;
 }
 
-console.log(humanReadable(90))
+console.log(humanReadable(3600));
+
+// function test(seconds) {
+//   var hours = parseInt(seconds / 3600);
+//   var minutes = parseInt(seconds / 60) % 60;
+//   var seconds = seconds % 60;
+
+//   var pad = function (val) {
+//     return val < 10 ? "0" + val : val;
+//   };
+
+//   return pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
+// }
+
+// console.log(test(3599));
