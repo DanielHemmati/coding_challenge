6070ba66222db8c0bd2f7e0ad6cad37366fac12d
DanielCodex <danialups1997@gmail.com>
2022-09-16 14:10:03 +0200

    type(x) == int, that was new for me in python

---
+++ codewar/7-kyu/list_filter.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/python
+
+def filter_list(l: list):
+    return list(filter(lambda x: True if type(x) == int else False, l))
+
+
+# ----------------------------------- other ---------------------------------- #
+def second(l: list) ->list:
+    return [i for i in l if isinstance(i, int)]
+
+a = [1, 2, 'a', 'b']
+print(second(a))
+
