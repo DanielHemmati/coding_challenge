33fd9dceaf082cf3c04b4af758c884cc729c0f16
DanielHemmati <danialups1997@gmail.com>
2025-03-19 14:37:37 +0100

    well interesting stuff in go

---
+++ codewar/7-kyu/vowelCount/vowelCount.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func getCount(str string) (count int) {
+	vowels := map[rune]bool{
+		'a': true, 'e': true, 'i': true, 'o': true, 'u': true,
+		'A': true, 'E': true, 'I': true, 'O': true, 'U': true,
+	}
+
+	count = 0
+	for _, c := range str {
+		if vowels[c] {
+			count += 1
+		}
+	}
+	fmt.Println(count)
+	return count
+}
+
+func main() {
+	// getCount("abracadabra")
+	// fmt.Println(secondSol("abracadabra"))
+	// exampleStringsCount()
+	fmt.Println(usingRegex("abracadabra"))
+
+}
+
+// solutions
+
+func secondSol(str string) int {
+	count := 0
+
+	vowels := []string{"a", "e", "i", "o", "u"}
+
+	for _, vowel := range vowels {
+		count += strings.Count(str, vowel)
+	}
+
+	return count
+}
+
+// nice
+func exampleStringsCount() {
+	str := "hello world"
+	substr := "l"
+	count := strings.Count(str, substr)
+	fmt.Printf("The substring '%s' appears %d times in '%s'\n", substr, count, str)
+}
+
+func usingRegex(str string) int {
+	r := regexp.MustCompile("[aeiou]")
+	vowels := r.FindAllString(str, -1)
+	return len(vowels)
+}
