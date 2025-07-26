7f975988b8a8d935e74de1e212623d5ef333bd09
DanielHemmati <danialups1997@gmail.com>
2025-07-26 11:07:35 +0200

    ship🚀

---
+++ codewar/7-kyu/strings_end_with.php
@@ -0,0 +1,12 @@
+<?php
+
+// https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/php
+
+function solution(string $str, string $ending): bool
+{
+    return str_ends_with($str, $ending);
+}
+
+$haystack = 'abc';
+$needle = 'bc';
+var_dump(solution($haystack, $needle));
