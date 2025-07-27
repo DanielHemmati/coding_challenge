7cf65927e5b450e98f397212b95b587f1cc6d7d6
DanielHemmati <danialups1997@gmail.com>
2025-07-27 12:18:39 +0200

    ship🚀

--- codewar/6-kyu/unique_in_order.php
+++ codewar/6-kyu/unique_in_order.php
@@ -2,10 +2,39 @@
 
 // https://www.codewars.com/kata/54e6533c92449cc251001667/train/php
 
+// didin't solved
 function uniqueInOrder(string $iterable): array
 {
-    return array_unique(str_split($iterable));
+    if (is_array($iterable)) {
+        $iterable = implode('', $iterable);
+    }
+
+    $res = [];
+    $prev = null;
+
+    foreach (str_split($iterable) as $char) {
+        if ($char !== $prev) {
+            $res[] = $char;
+            $prev = $char;
+        }
+    }
+    return $res;
 }
 
 $a = 'AAAABBBCCDAABBB';
-var_dump(uniqueInOrder($a));
+// var_dump(uniqueInOrder($a));
+
+//------------- solutions -------------
+
+function sol1($iterable)
+{
+    $arr = is_string($iterable) ? str_split($iterable) : $iterable;
+    $ret = array_reduce($arr, function($carry, $item) {
+        if ($item !== end($carry)){
+            $carry[] = $item;
+        }
+        return $carry;
+    }, []);
+
+    return $ret;
+}
