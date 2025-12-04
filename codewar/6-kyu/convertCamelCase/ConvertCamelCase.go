e91db409f0096aa23c59420c87a885aa3ac0e48c
DanielHemmati <danialups1997@gmail.com>
2025-12-04 10:39:45 +0100

    nice problem for go

---
+++ codewar/6-kyu/convertCamelCase/ConvertCamelCase.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode"
+)
+
+func IsFirstCharUpper(s string) bool {
+	if s == "" {
+		return false
+	}
+	r := []rune(s)[0]
+	return unicode.IsUpper(r) && unicode.IsLetter(r)
+}
+
+func Capitalize(word string) string {
+	if word == "" {
+		return word
+	}
+
+	runes := []rune(word)
+	runes[0] = unicode.ToUpper(runes[0])
+	return string(runes)
+}
+
+func ToCamelCase(s string) string {
+	normalize := strings.FieldsFunc(s, func(r rune) bool {
+		return r == '-' || r == '_'
+	})
+
+	var sb strings.Builder
+
+	for _, word := range normalize {
+		sb.WriteString(Capitalize(word))
+	}
+
+	result := sb.String()
+	runes := []rune(result)
+
+	if len(runes) == 0 {
+		return ""
+	}
+
+	origUpper := IsFirstCharUpper(s)
+
+	// if the first char in original string is not cap
+	// then make the rune lower
+	if !origUpper {
+		runes[0] = unicode.ToLower(runes[0])
+	}
+
+	// otherwise just return what we have done in the loop
+	return string(runes)
+}
+
+func main() {
+	a := "the-stealth-warrior"
+	b := "The_Stealth_Warrior"
+	fmt.Println(ToCamelCase(a))
+	fmt.Println(ToCamelCase(b))
+
+	res := regexp.MustCompile("-|_").Split(a, -1)
+	fmt.Println(res)
+}
+
+// solutios
+// other solution were using strings.title which is deprecated
